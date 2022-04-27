8a2ca2baf87fe243a2d638cbd931be5b8b2f5b06
Josh Johnson <jcjohnson77@gmail.com>
2022-04-27 18:26:35 -0400

    Remove debug code

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,6 @@ import React from "react";
 import { ClickToComponent } from "click-to-react-component";
 
 export const wrapRootElement = ( {element }, pluginOptions ) => {
-  console.log(pluginOptions)
   const component = pluginOptions.enabled
     ? <ClickToComponent {...pluginOptions} />
     : null;
